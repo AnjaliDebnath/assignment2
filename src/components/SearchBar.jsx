c824b334b9bc402052eb32999445c46d8490b2e6
AnjaliDebnath <anjalidebnath1003@gmail.com>
2024-12-05 18:52:06 +0530

    changes added

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './SearchBar.css';
+import { CiSearch } from "react-icons/ci";
+import { CiLocationOn } from "react-icons/ci";
+import { MdMyLocation } from "react-icons/md";
 
 const SearchBar = () => {
   return (
     <div className="search-bar">
       {/* Job Search Input */}
       <div className="input-container">
-        <span className="icon">🔍</span>
+        <span className="icon"><CiSearch /></span>
         <input
           type="text"
           placeholder="Search by: Job title, Position, Keyword..."
@@ -16,14 +19,15 @@ const SearchBar = () => {
 
       {/* Location Input */}
       <div className="input-container">
-        <span className="icon">📍</span>
+        <span className="icon"><CiLocationOn /></span>
         <input
           type="text"
           placeholder="City, state or country"
           className="input-field"
         />
       </div>
-
+      <MdMyLocation />
+      
       {/* Find Job Button */}
       <button className="find-job-button">Find Job</button>
     </div>
